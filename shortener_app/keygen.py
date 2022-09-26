355c97e335873fe847b8e1d5c1c38792d173319f
Grant Ramsay <seapagan@gmail.com>
2022-09-26 14:16:02 +0100

    Add lower case letters to the key generation
    Signed-off-by: Grant Ramsay <seapagan@gmail.com>

--- shortener_app/keygen.py
+++ shortener_app/keygen.py
@@ -9,7 +9,7 @@ from . import crud
 
 def create_random_key(length: int = 5) -> str:
     """Return a random key of the specified length."""
-    chars = string.ascii_uppercase + string.digits
+    chars = string.ascii_uppercase + string.ascii_lowercase + string.digits
     return "".join(secrets.choice(chars) for _ in range(length))
 
 
